a6fab00167db231b2cf8714f7c1ad2fbe5b52182
lordrichard6 <paulolopesreizinho@gmail.com>
2024-05-01 22:34:00 +0200

    task: projects mockup done

--- src/_root/pages/Home.jsx
+++ src/_root/pages/Home.jsx
@@ -6,7 +6,7 @@ import { SiGithub } from "react-icons/si";
 const Home = () => {
   return (
     <>
-      <header id="top" className='w-screen min-h-screen max-h-screen relative flex justify-center items-center py-24 mb-40'>
+      <header id="top" className='max-h-screen relative flex justify-center items-center mb-40'>
         <div className="section-width-default flex-centered w-full flex-col-reverse xl:flex-row z-20 relative">
           <div className="flex-centered absolute z-10 md:w-[50vw] lg:w-2/3l xl:w-5/12 px-4 md:px-0 max-h-[80vh] max-w-[700px]">
             <img className='w-full p-0 xl:p-6 lg:mx-auto' src="/src/assets/images/me_crossed_arms.png" alt="" />
@@ -39,8 +39,8 @@ const Home = () => {
           </div>
           <div className="flex-centered">
             <button className='btn-primary'>Know more</button>
-            <button className='btn-social'><SlSocialLinkedin /></button>
-            <button className='btn-social'><SlSocialInstagram /></button>
+            <button className='btn-social pink'><SlSocialLinkedin /></button>
+            <button className='btn-social pink'><SlSocialInstagram /></button>
           </div>
         </div>
 
@@ -90,7 +90,7 @@ const Home = () => {
           </div>
           <div className="flex-centered mb-40">
             <button className='btn-primary'>More projects</button>
-            <button className='btn-social'><SiGithub /></button>
+            <button className='btn-social pink'><SiGithub /></button>
           </div>
         </div>
       </section>
--- src/_root/pages/Projects.jsx
+++ src/_root/pages/Projects.jsx
@@ -1,8 +1,150 @@
 import React from 'react'
+import { SiGithub } from "react-icons/si";
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <>
+      <header id="top" className='max-h-screen min-h-screen relative flex justify-center items-center mb-40'>
+        <h1 className='text-[220px]'>Projects</h1>
+      </header>
+
+      <section className="portfolio-intro section-width-default w-full flex-centered relative mb-40">
+        <div className="flex-centered flex-col">
+          <h1 className='mb-40'>Portfolio</h1>
+          <p>This is an exploration of font sizing and optimal  character limits per line—because you know, there is a limit. Each line, except the first, aims for a character length between ~70 and ~75 characters. </p>
+          <br />
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui modi, quis nisi incidunt quidem ducimus soluta doloribus minima laboriosam mollitia veniam quo asperiores sit aliquid quae? Nobis aliquid excepturi voluptatum.</p>
+        </div>
+      </section>
+
+      <section className="projects-main section-width-default min-h-screen w-full flex-centered flex-col relative mb-40">
+        <div className="tags-container grid grid-rows-2 grid-flow-col gap-4 mb-40">
+          <button className='btn-tag'>#tag</button>
+          <button className='btn-tag'>#tag</button>
+          <button className='btn-tag'>#tag</button>
+          <button className='btn-tag'>#tag</button>
+          <button className='btn-tag'>#tag</button>
+          <button className='btn-tag'>#tag</button>
+          <button className='btn-tag'>#tag</button>
+          <button className='btn-tag'>#tag</button>
+          <button className='btn-tag'>#tag</button>
+          <button className='btn-tag'>#tag</button>
+          <button className='btn-tag'>#tag</button>
+          <button className='btn-tag'>#tag</button>
+        </div>
+        <div className="projects-container">
+
+          <div className="project flex-centered bg-[#E1AFD1] dark:bg-[#610C9F] w-screen">
+            <div className="flex section-width-default flex-centered">
+              <div className="w-fit my-20">
+                <img className='object-cover min-w-[480px] h-[480px] rounded' src="/src/assets/images/test-dark.jpg" alt="" />
+              </div>
+              <div className="h-full flex flex-col justify-between ml-10">
+                <div className="flex justify-between">
+                  <h2>Project 1</h2>
+                  <div className="text-xl grid grid-flow-col gap-4">
+                    <span>#tag</span>
+                    <span>#tag</span>
+                    <span>#tag</span>
+                    <span>#tag</span>
+                    <span>#tag</span>
+                  </div>
+                </div>
+                <p>This is an exploration of font sizing and optimal  character limits per line—because you know, there is a limit. Each line, except the first, aims for a character length between ~70 and ~75 characters. </p>
+                <br />
+                <p>This is an exploration of font sizing and optimal  character limits per line—because you know, there is a limit. Each line, except the first, aims for a character length between ~70 and ~75 characters. </p>
+                <div className="w-full flex justify-end items-end mt-20">
+                  <button className='btn-project ml-auto'>Visit project</button>
+                  <button className='btn-social small'><SiGithub /></button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="project flex-centered bg-[#C4E4FF] dark:bg-[#00224D] w-screen">
+            <div className="flex section-width-default flex-centered">
+              <div className="w-fit my-20">
+                <img className='object-cover min-w-[480px] h-[480px] rounded' src="/src/assets/images/test-dark.jpg" alt="" />
+              </div>
+              <div className="h-full flex flex-col justify-between ml-10">
+                <div className="flex justify-between">
+                  <h2>Project 2</h2>
+                  <div className="text-xl grid grid-flow-col gap-4">
+                    <span>#tag</span>
+                    <span>#tag</span>
+                    <span>#tag</span>
+                    <span>#tag</span>
+                    <span>#tag</span>
+                  </div>
+                </div>
+                <p>This is an exploration of font sizing and optimal  character limits per line—because you know, there is a limit. Each line, except the first, aims for a character length between ~70 and ~75 characters. </p>
+                <br />
+                <p>This is an exploration of font sizing and optimal  character limits per line—because you know, there is a limit. Each line, except the first, aims for a character length between ~70 and ~75 characters. </p>
+                <div className="w-full flex justify-end items-end mt-20">
+                  <button className='btn-project pink ml-auto'>Visit project</button>
+                  <button className='btn-social pink small'><SiGithub /></button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="project flex-centered bg-[#E1AFD1] dark:bg-[#610C9F] w-screen">
+            <div className="flex section-width-default flex-centered">
+              <div className="w-fit my-20">
+                <img className='object-cover min-w-[480px] h-[480px] rounded' src="/src/assets/images/test-dark.jpg" alt="" />
+              </div>
+              <div className="h-full flex flex-col justify-between ml-10">
+                <div className="flex justify-between">
+                  <h2>Project 3</h2>
+                  <div className="text-xl grid grid-flow-col gap-4">
+                    <span>#tag</span>
+                    <span>#tag</span>
+                    <span>#tag</span>
+                    <span>#tag</span>
+                    <span>#tag</span>
+                  </div>
+                </div>
+                <p>This is an exploration of font sizing and optimal  character limits per line—because you know, there is a limit. Each line, except the first, aims for a character length between ~70 and ~75 characters. </p>
+                <br />
+                <p>This is an exploration of font sizing and optimal  character limits per line—because you know, there is a limit. Each line, except the first, aims for a character length between ~70 and ~75 characters. </p>
+                <div className="w-full flex justify-end items-end mt-20">
+                  <button className='btn-project ml-auto'>Visit project</button>
+                  <button className='btn-social small'><SiGithub /></button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="project flex-centered bg-[#C4E4FF] dark:bg-[#00224D] w-screen">
+            <div className="flex section-width-default flex-centered">
+              <div className="w-fit my-20">
+                <img className='object-cover min-w-[480px] h-[480px] rounded' src="/src/assets/images/test-dark.jpg" alt="" />
+              </div>
+              <div className="h-full flex flex-col justify-between ml-10">
+                <div className="flex justify-between">
+                  <h2>Project 4</h2>
+                  <div className="text-xl grid grid-flow-col gap-4">
+                    <span>#tag</span>
+                    <span>#tag</span>
+                    <span>#tag</span>
+                    <span>#tag</span>
+                    <span>#tag</span>
+                  </div>
+                </div>
+                <p>This is an exploration of font sizing and optimal  character limits per line—because you know, there is a limit. Each line, except the first, aims for a character length between ~70 and ~75 characters. </p>
+                <br />
+                <p>This is an exploration of font sizing and optimal  character limits per line—because you know, there is a limit. Each line, except the first, aims for a character length between ~70 and ~75 characters. </p>
+                <div className="w-full flex justify-end items-end mt-20">
+                  <button className='btn-project pink ml-auto'>Visit project</button>
+                  <button className='btn-social pink small'><SiGithub /></button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </section>
+    </>
   )
 }
 
