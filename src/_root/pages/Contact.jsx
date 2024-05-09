74c857db105d5bb099988f003b5036ba50237aca
lordrichard6 <paulolopesreizinho@gmail.com>
2024-05-09 19:05:01 +0200

    task: add contact form functionality

--- src/_root/pages/Contact.jsx
+++ src/_root/pages/Contact.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { useState, useEffect, useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 import { SlSocialLinkedin, SlSocialInstagram } from "react-icons/sl";
 import { SiGithub, SiWhatsapp } from "react-icons/si";
@@ -12,6 +13,37 @@ import homeIcon from '../../assets/icons/home.svg'
 import handIcon from '../../assets/icons/hand.svg'
 
 const Contact = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [stateMessage, setStateMessage] = useState(null);
+
+  const handleOnSubmit = (e) => {
+    const publicKey = process.env.EMAILJS_PUBLIC_KEY
+    const serviceId = process.env.EMAILJS_SERVICE_ID
+    const templateId = process.env.EMAILJS_TEMPLATE_ID
+
+    e.persist();
+    e.preventDefault();
+    setIsSubmitting(true);
+    emailjs.sendForm(serviceId, templateId, e.target, publicKey)
+      .then((result) => {
+        setStateMessage('Message sent!');
+        setIsSubmitting(false);
+        console.log(result.text);
+        setTimeout(() => {
+          setStateMessage(null);
+        }, 5000); // hide message after 5 seconds
+
+      }, (error) => {
+        setStateMessage('Something went wrong, please try again later');
+        console.log(error.text);
+        setIsSubmitting(false);
+        setTimeout(() => {
+          setStateMessage(null);
+        }, 5000); // hide message after 5 seconds
+      });
+    e.target.reset()
+  };
+
   return (
     <>
       <header id="top" className='contact-header flex-centered h-[80vh] lg:h-[70vh] w-full relative'>
@@ -49,29 +81,29 @@ const Contact = () => {
           </div>
 
           <div className="form-container w-full lg:w-1/2 h-full">
-            <form className='flex flex-col justify-between absolute left-1/2 lg:left-auto -translate-x-1/2 lg:translate-x-0 lg:top-64 bg-light-1 dark:bg-dark-3 w-[376px] h-[654px] lg:w-[476px] lg:h-[754px] p-10' action="">
+            <form onSubmit={handleOnSubmit} className='flex flex-col justify-between absolute left-1/2 lg:left-auto -translate-x-1/2 lg:translate-x-0 lg:top-64 bg-light-1 dark:bg-dark-3 w-[376px] h-[654px] lg:w-[476px] lg:h-[754px] p-10' action="">
               <h3>Send me a message</h3>
               <fieldset>
-                <label for="name">Your name</label>
-                <input type="text" id="name" name="name" placeholder="Ex: Jordan Peterson" required />
+                <label htmlFor="from_name">Your name</label>
+                <input id="from_name" name="from_name" type="text" placeholder="Ex: Jordan Peterson" required />
               </fieldset>
 
               <fieldset>
-                <label for="email">Your email</label>
-                <input type="email" id="email" name="email" placeholder="Ex: paulolopes@gmail.com" required />
+                <label htmlFor="from_email">Your email</label>
+                <input id="from_email" name="from_email" type="email" placeholder="Ex: paulolopes@gmail.com" required />
               </fieldset>
 
               <fieldset>
-                <label for="phone">Your phone number</label>
-                <input type="tel" id="phone" name="phone" placeholder="Ex: +41 078 798 95 33" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required />
+                <label htmlFor="from_phone">Your phone number</label>
+                <input type="tel" id="from_phone" name="from_phone" placeholder="Ex: +41 078 798 95 33" required />
               </fieldset>
 
               <fieldset>
-                <label for="message">Comments</label>
+                <label htmlFor="message">Comments</label>
                 <textarea id="message" name="message" rows="6" placeholder="What do you need help with? What challenges you have?" required></textarea>
               </fieldset>
-
-              <button className='btn small blue' type='submit'>Send</button>
+              {stateMessage && <span>{stateMessage}</span>}
+              <button className='btn small blue' type='submit' value="Send" disabled={isSubmitting} >Send</button>
             </form>
           </div>
         </div>
